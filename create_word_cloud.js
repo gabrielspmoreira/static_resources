30da03e9ce0e46c852a4f33d5f8e94b2f36480da
Gabriel Moreira <gabrielpm@ciandt.com>
2015-05-10 09:23:25 -0300

    update

--- create_word_cloud.js
+++ create_word_cloud.js
@@ -1,38 +1,40 @@
-$( ".smartconnect_wordcloud" ).each(function() {
-    console.log($(this).attr("data-userid"));
-    cloud_dom_element = $(this).get(0);
+$( document ).ready(function() {
+    $( ".smartconnect_wordcloud" ).each(function() {
+        console.log($(this).attr("data-userid"));
+        cloud_dom_element = $(this).get(0);
 
-    relevantTermsRaw = $(this).attr("data-relevantterms");
+        relevantTermsRaw = $(this).attr("data-relevantterms");
 
-    if (!relevantTermsRaw) 
-        return;
+        if (!relevantTermsRaw) 
+            return;
 
-    relevantTerms = relevantTermsRaw.split(',');
+        relevantTerms = relevantTermsRaw.split(',');
 
-    var tags = [];
-    relevantTerms.slice(0,50).forEach(function(term) {
-        splittedTerm = term.split('|');
-        tags.push([splittedTerm[0],parseInt(splittedTerm[1])]);
+        var tags = [];
+        relevantTerms.slice(0,50).forEach(function(term) {
+            splittedTerm = term.split('|');
+            tags.push([splittedTerm[0],parseInt(splittedTerm[1])]);
+        });
+
+        tags_list = tags.map(function(word) { return [word[0], Math.round(word[1]/2)]; })
+        console.log(tags_list);
+
+        WordCloud(cloud_dom_element, {
+          gridSize: 12, 
+          weightFactor: 2, 
+          rotateRatio: 0.5,
+          list : tags_list, 
+          wait: 10
+        });
+
+        var clicked = function(ev) {
+          if (ev.target.nodeName === "SPAN") {
+            var tag = ev.target.textContent;
+
+            window.location.href = "http://d-coder.smartcanvas.com/"+tag.replace(' ','+');
+
+          }
+        }
+        cloud_dom_element.addEventListener("click", clicked);
     });
-
-    tags_list = tags.map(function(word) { return [word[0], Math.round(word[1]/2)]; })
-    console.log(tags_list);
-
-    WordCloud(cloud_dom_element, {
-      gridSize: 12, 
-      weightFactor: 2, 
-      rotateRatio: 0.5,
-      list : tags_list, 
-      wait: 10
-    });
-
-    var clicked = function(ev) {
-      if (ev.target.nodeName === "SPAN") {
-        var tag = ev.target.textContent;
-
-        window.location.href = "http://d-coder.smartcanvas.com/"+tag.replace(' ','+');
-
-      }
-    }
-    cloud_dom_element.addEventListener("click", clicked);
 });
