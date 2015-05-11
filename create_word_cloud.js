7253500820149d20c33940e0f55cb1b24fd77c10
Gabriel Moreira <gabrielpm@ciandt.com>
2015-05-11 10:39:19 -0300

    Avoiding multiple processing

--- create_word_cloud.js
+++ create_word_cloud.js
@@ -1,7 +1,7 @@
 $( window ).load(function() {
     $( ".smartconnect_wordcloud" ).each(function() {      
         console.log($(this).attr("data-processed"));
-        if ($(this).attr("data-processed")) return;
+        //if ($(this).attr("data-processed")) return;
 
         $(this).attr("data-processed","true");
 
