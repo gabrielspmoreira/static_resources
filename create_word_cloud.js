baeeb1b80792955c48b29ccd44dab6445a007bc2
Gabriel Moreira <gabrielpm@ciandt.com>
2015-05-11 09:30:58 -0300

    Avoiding multiple processing

--- create_word_cloud.js
+++ create_word_cloud.js
@@ -1,14 +1,18 @@
 $( window ).load(function() {
-    $( ".smartconnect_wordcloud" ).each(function() {
-        console.log($(this).attr("data-userid"));
-        cloud_dom_element = $(this).get(0);
+    if (window.alreadyProcessingWordClouds) return;
 
-        relevantTermsRaw = $(this).attr("data-relevantterms");
+    window.alreadyProcessingWordClouds = true;
+
+    $( ".smartconnect_wordcloud" ).each(function() {        
+        console.log('Setting wordclouds for '+$(this).attr("data-userid"));
+        var cloud_dom_element = $(this).get(0);
+
+        var relevantTermsRaw = $(this).attr("data-relevantterms");
 
         if (!relevantTermsRaw) 
             return;
 
-        relevantTerms = relevantTermsRaw.split(',');
+        var relevantTerms = relevantTermsRaw.split(',');
 
         var tags = [];
         relevantTerms.slice(0,50).forEach(function(term) {
@@ -16,8 +20,7 @@ $( window ).load(function() {
             tags.push([splittedTerm[0],parseInt(splittedTerm[1])]);
         });
 
-        tags_list = tags.map(function(word) { return [word[0], Math.round(word[1]/3)]; })
-        console.log(tags_list);
+        var tags_list = tags.map(function(word) { return [word[0], Math.round(word[1]/3)]; })
 
         WordCloud(cloud_dom_element, {
           gridSize: 12, 
@@ -31,7 +34,7 @@ $( window ).load(function() {
           if (ev.target.nodeName === "SPAN") {
             var tag = ev.target.textContent;
 
-            window.location.href = "http://d-coder.smartcanvas.com/"+tag.replace(' ','+');
+            window.location.href = "/"+tag.replace(' ','+');
 
           }
         }
